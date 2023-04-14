75ca8ed32926e8557afebdb5dba5b306d6ef202b
Abril Aguilar Lopez <abril124@ucla.edu>
2023-04-13 20:48:20 -0700

    generate image imports bc im dumb

---
+++ src/create.py
@@ -0,0 +1,8 @@
+i = 1
+while  i != 52 :
+        print("import f"+str(i)+" from \'./faces/female/F ("+str(i)+").jpg\'"),
+        i = i+1
+i = 1
+while  i != 52 :
+    print( 'f'+str(i) +', ');
+    i+=1
