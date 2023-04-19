277e0d0440ec8131eab6f4bb0d7c69c56c3267d3
Abril Aguilar Lopez <abril124@ucla.edu>
2023-04-18 23:52:12 -0700

    rand order

--- src/Pics.js
+++ src/Pics.js
@@ -133,7 +133,7 @@ import m44 from './faces/male/M (44).jpg'
 
 
 // f = 0-50  m 51-77 (?)
-var images = [f1, f2, f3, f4, f5, f6, f7, f8, f9, f10,
+export var images = [f1, f2, f3, f4, f5, f6, f7, f8, f9, f10,
     f11, f12, f13, f14, f15, f16, f17, f18, f19, f20,
     f21, f22, f23, f24, f25, f26, f27, f28, f29, f30,
     f31, f32, f33, f34, f35, f36, f37, f38, f39, f40,
@@ -148,19 +148,20 @@ var images = [f1, f2, f3, f4, f5, f6, f7, f8, f9, f10,
     m31, m32, m33, m34, m35, m36, m37, m38, m39, m40,
     m41, m42, m43, m44];//doesnt scale well, but oh well
 
+export var test = [];
 
 function Pics(user) {
-    console.log(user['user'][2]);
-    const prefRange = get_pref_imgs(user['user'][2]);
+    const SO = user['user'][2]
+    const prefRange = get_pref_imgs(SO);
     const prefImgs = images.slice(prefRange['l'], prefRange['h'] + 1);
-    const [currentImage, setCurrentImage] = useState(prefImgs[0]);
+    const [randOrder, setRandOrd] = useState(getRandImgs(SO));
     const [i, seti] = useState(0);
     const [b, setb] = useState(false)
     const TIME = 500;
     if (b) {
         if (i == 10) {
             return (<>
-                <PostTrial user={user}></PostTrial>
+                <PostTrial user={user} imgs={randOrder}></PostTrial>
             </>);
         }
         return (
@@ -182,17 +183,22 @@ function Pics(user) {
     else {
         return (
             <div>
-                <Countdown width={368} tgt={TIME} event={() => { setCurrentImage(prefImgs[i + 1]); seti(i + 1); setb(true); }} ></Countdown>
+                <Countdown width={368} tgt={TIME} event={() => { seti(i + 1); setb(true); }} ></Countdown>
                 <div>
-                    <Image height={400} src={currentImage}  ></Image>
+                    <Image height={400} src={images[randOrder[i]]}  ></Image>
                 </div>
             </div>
         );
     }
 }
+/* not rand img
+                <div>
+                    <Image height={400} src={currentImage}  ></Image>
+                </div>
+*/
 
 function get_pref_imgs(so) {
-    console.log(so);
+    //console.log(so);
     const MID = 80;
     const MAX = 123;
     if (so != '') {
@@ -206,6 +212,48 @@ function get_pref_imgs(so) {
     else {
         console.log('error with pref prop');
         return null;
+    }
+}
+
+function getRandImgs(so) {
+    const MID = 80;
+    const MAX = 123;
+    const list = [];
+    if (so != '') {
+        if (so === "Heterosexual (female)" || so === "Homosexual (male)") {
+            while (list.length < 10) {
+
+                const randomNumber = Math.floor(Math.random() * (MAX - MID + 1)) + MID;
+                if (!list.includes(randomNumber)) {
+                    list.push(randomNumber);
+                }
+            }
+        }
+        if (so === "Heterosexual (male)" || so === "Homosexual (female)") {
+            while (list.length < 10) {
+                const randomNumber = Math.floor(Math.random() * (MID + 1));
+                if (!list.includes(randomNumber)) {
+                    list.push(randomNumber);
+                }
+            }
+        }
+        if (so === "Bisexual/pansexual" || so === "Asexual/other") {
+            while (list.length < 10) {
+                const randomNumber = Math.floor(Math.random() * (MAX + 1));
+                if (!list.includes(randomNumber)) {
+                    list.push(randomNumber);
+                }
+            }
+        }
+        //console.log(list);
+        test.push(list)
+        return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        //return list;
+    }
+    else {
+        console.log('error with pref prop');
+        return [];
     }
 }
 
--- src/PostTrial.js
+++ src/PostTrial.js
@@ -9,12 +9,21 @@ import Button from 'react-bootstrap/Button';
 import { Card, Badge } from "react-bootstrap";
 
 import { addTrial } from './firebase';
+import { test } from "./Pics";
+import { images } from "./Pics";
 
-function PostTrial(user) {
+function PostTrial(user, rand) {
     const [q1, setq1] = useState('');
     const [q2, setq2] = useState('');
     const [q3, setq3] = useState('');
     const [fin, finish] = useState(false);
+    const [swipe_dict, setSD] = useState(['', '', '', '', '', '', '', '', '', '']);
+    //console.log(test[0]);
+    function updateSwipes(i, value) {
+        const newSwipeDict = [...swipe_dict];
+        newSwipeDict[i] = value;
+        setSD(newSwipeDict);
+    }
     if (fin)
         return (
             <>
@@ -51,7 +60,211 @@ function PostTrial(user) {
                         <input name="myInput" onChange={e => { setq3(e.target.value); }} />
                     </Form.Group>
                 </Form>
-                <Button onClick={() => finish(sendData(user['user']['user'], q1, q2, q3))} >
+                <button onClick={() => console.log(test[0])}>test</button>
+                <span id="content1"></span>
+                <text>Please mark how you would "swipe" for each image</text>
+                <Form>
+                    {['radio'].map((type) => (
+                        <div key={`inline-${type}`} className="mb-3">
+                            <img height={50} src={images[test[0][0]]}></img>
+                            <span id="content1"></span>
+                            <Form.Check
+                                inline label="Left" name="group1" type={type} id={`inline-${type}-s1`}
+                                onChange={() => updateSwipes(0, 'Left')}
+                            />
+                            <Form.Check
+                                inline label="Right" name="group1" type={type} id={`inline-${type}-2`}
+                                onChange={() => updateSwipes(0, 'Right')}
+                            />
+                            <Form.Check
+                                inline label="Neither/Neutral" type={type} id={`inline-${type}-3`}
+                                onChange={() => updateSwipes(0, 'Neither')}
+                            />
+                        </div>
+                    ))}
+                </Form>
+                <Form>
+                    {['radio'].map((type) => (
+                        <div key={`inline-${type}`} className="mb-3">
+                            <img height={50} src={images[test[0][1]]}></img>
+                            <span id="content1"></span>
+                            <Form.Check
+                                inline label="Left" name="group1" type={type} id={`inline-${type}-1`}
+                                onChange={() => updateSwipes(1, 'Left')}
+                            />
+                            <Form.Check
+                                inline label="Right" name="group1" type={type} id={`inline-${type}-2`}
+                                onChange={() => updateSwipes(1, 'Right')}
+                            />
+                            <Form.Check
+                                inline label="Neither/Neutral" type={type} id={`inline-${type}-3`}
+                                onChange={() => updateSwipes(1, 'Neither')}
+                            />
+                        </div>
+                    ))}
+                </Form>
+                <Form>
+                    {['radio'].map((type) => (
+                        <div key={`inline-${type}`} className="mb-3">
+                            <img height={50} src={images[test[0][2]]}></img>
+                            <span id="content1"></span>
+                            <Form.Check
+                                inline label="Left" name="group1" type={type} id={`inline-${type}-1`}
+                                onChange={() => updateSwipes(2, 'Left')}
+                            />
+                            <Form.Check
+                                inline label="Right" name="group1" type={type} id={`inline-${type}-2`}
+                                onChange={() => updateSwipes(2, 'Right')}
+                            />
+                            <Form.Check
+                                inline label="Neither/Neutral" type={type} id={`inline-${type}-3`}
+                                onChange={() => updateSwipes(2, 'Neither')}
+                            />
+                        </div>
+                    ))}
+                </Form>
+                <Form>
+                    {['radio'].map((type) => (
+                        <div key={`inline-${type}`} className="mb-3">
+                            <img height={50} src={images[test[0][3]]}></img>
+                            <span id="content1"></span>
+                            <Form.Check
+                                inline label="Left" name="group1" type={type} id={`inline-${type}-1`}
+                                onChange={() => updateSwipes(3, 'Left')}
+                            />
+                            <Form.Check
+                                inline label="Right" name="group1" type={type} id={`inline-${type}-2`}
+                                onChange={() => updateSwipes(3, 'Right')}
+                            />
+                            <Form.Check
+                                inline label="Neither/Neutral" type={type} id={`inline-${type}-3`}
+                                onChange={() => updateSwipes(3, 'Neither')}
+                            />
+                        </div>
+                    ))}
+                </Form>
+                <Form>
+                    {['radio'].map((type) => (
+                        <div key={`inline-${type}`} className="mb-3">
+                            <img height={50} src={images[test[0][4]]}></img>
+                            <span id="content1"></span>
+                            <Form.Check
+                                inline label="Left" name="group1" type={type} id={`inline-${type}-1`}
+                                onChange={() => updateSwipes(4, 'Left')}
+                            />
+                            <Form.Check
+                                inline label="Right" name="group1" type={type} id={`inline-${type}-2`}
+                                onChange={() => updateSwipes(4, 'Right')}
+                            />
+                            <Form.Check
+                                inline label="Right" name="group1" type={type} id={`inline-${type}-2`}
+                                onChange={() => updateSwipes(4, 'Neither')}
+                            />
+                        </div>
+                    ))}
+                </Form>
+                <Form>
+                    {['radio'].map((type) => (
+                        <div key={`inline-${type}`} className="mb-3">
+                            <img height={50} src={images[test[0][5]]}></img>
+                            <span id="content1"></span>
+                            <Form.Check
+                                inline label="Left" name="group1" type={type} id={`inline-${type}-1`}
+                                onChange={() => updateSwipes(5, 'Left')}
+                            />
+                            <Form.Check
+                                inline label="Right" name="group1" type={type} id={`inline-${type}-2`}
+                                onChange={() => updateSwipes(5, 'Right')}
+                            />
+                            <Form.Check
+                                inline label="Neither/Neutral" type={type} id={`inline-${type}-3`}
+                                onChange={() => updateSwipes(5, 'Neither')}
+                            />
+                        </div>
+                    ))}
+                </Form>
+                <Form>
+                    {['radio'].map((type) => (
+                        <div key={`inline-${type}`} className="mb-3">
+                            <img height={50} src={images[test[0][6]]}></img>
+                            <span id="content1"></span>
+                            <Form.Check
+                                inline label="Left" name="group1" type={type} id={`inline-${type}-1`}
+                                onChange={() => updateSwipes(6, 'Left')}
+                            />
+                            <Form.Check
+                                inline label="Right" name="group1" type={type} id={`inline-${type}-2`}
+                                onChange={() => updateSwipes(6, 'Right')}
+                            />
+                            <Form.Check
+                                inline label="Neither/Neutral" type={type} id={`inline-${type}-3`}
+                                onChange={() => updateSwipes(6, 'Neither')}
+                            />
+                        </div>
+                    ))}
+                </Form>
+                <Form>
+                    {['radio'].map((type) => (
+                        <div key={`inline-${type}`} className="mb-3">
+                            <img height={50} src={images[test[0][7]]}></img>
+                            <span id="content1"></span>
+                            <Form.Check
+                                inline label="Left" name="group1" type={type} id={`inline-${type}-1`}
+                                onChange={() => updateSwipes(7, 'Left')}
+                            />
+                            <Form.Check
+                                inline label="Right" name="group1" type={type} id={`inline-${type}-2`}
+                                onChange={() => updateSwipes(7, 'Right')}
+                            />
+                            <Form.Check
+                                inline label="Neither/Neutral" type={type} id={`inline-${type}-3`}
+                                onChange={() => updateSwipes(7, 'Neither')}
+                            />
+                        </div>
+                    ))}
+                </Form>
+                <Form>
+                    {['radio'].map((type) => (
+                        <div key={`inline-${type}`} className="mb-3">
+                            <img height={50} src={images[test[0][8]]}></img>
+                            <span id="content1"></span>
+                            <Form.Check
+                                inline label="Left" name="group1" type={type} id={`inline-${type}-1`}
+                                onChange={() => updateSwipes(8, 'Left')}
+                            />
+                            <Form.Check
+                                inline label="Right" name="group1" type={type} id={`inline-${type}-2`}
+                                onChange={() => updateSwipes(8, 'Right')}
+                            />
+                            <Form.Check
+                                inline label="Neither/Neutral" type={type} id={`inline-${type}-3`}
+                                onChange={() => updateSwipes(8, 'Neither')}
+                            />
+                        </div>
+                    ))}
+                </Form>
+                <Form>
+                    {['radio'].map((type) => (
+                        <div key={`inline-${type}`} className="mb-3">
+                            <img height={50} src={images[test[0][9]]}></img>
+                            <span id="content1"></span>
+                            <Form.Check
+                                inline label="Left" name="group1" type={type} id={`inline-${type}-1`}
+                                onChange={() => updateSwipes(9, 'Left')}
+                            />
+                            <Form.Check
+                                inline label="Right" name="group1" type={type} id={`inline-${type}-2`}
+                                onChange={() => updateSwipes(9, 'Right')}
+                            />
+                            <Form.Check
+                                inline label="Neither/Neutral" type={type} id={`inline-${type}-3`}
+                                onChange={() => updateSwipes(9, 'Neither')}
+                            />
+                        </div>
+                    ))}
+                </Form>
+
+                <Button onClick={() => finish(sendData(user['user']['user'], q1, q2, q3, swipe_dict))} >
                     Submit
                 </Button >
             </>
@@ -59,14 +272,22 @@ function PostTrial(user) {
     }
 }
 
-function sendData(user, q1, q2, q3) {
-    console.log(user);
+function sendData(user, q1, q2, q3, swipe_dict) {
+    console.log(swipe_dict);
+    const randInd = test[0];
+    const sendDict = {};
+    const i = 0;
+    while (sendDict.length < 10) {
+        sendDict[randInd[i]] = swipe_dict[i];
+        i += 1;
+    }
+    console.log(sendDict)
     user.push(q1);
     user.push(q2);
     user.push(q3);
     const send = {
         Name: user[0], gender: user[1], sexual_orientation: user[2], addition_info: user[3],
-        describe_type: user[4], stuck_out: user[5], recognize: user[6]
+        describe_type: user[4], stuck_out: user[5], recognize: user[6], swipe: sendDict
     }
     addTrial(send);
     return true;
